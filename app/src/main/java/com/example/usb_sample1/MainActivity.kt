5c105783ed150d4396e67790ad8e84d69f193952
eridenb <eridenboriga@gmail.com>
2022-01-05 14:56:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/usb_sample1/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.usb_sample1
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbAccessory
+import android.hardware.usb.UsbManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var accessory: UsbAccessory
+    private var usbManager: UsbManager? = null
+    private var usbDeviceStateFilter : IntentFilter? = null
+    private var fileDescriptor: ParcelFileDescriptor? = null
+    private var inputStream: FileInputStream? = null
+    private var outputStream: FileOutputStream? = null
+    private val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var textview = findViewById<TextView>(R.id.textView)
+        //textview.text = "USB接続せれておりません！"
+
+        val usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+
+        usbDeviceStateFilter?.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
+        usbDeviceStateFilter?.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED)
+        //registerReceiver(usbReceiver,usbDeviceStateFilter)
+
+        val filter = IntentFilter(ACTION_USB_PERMISSION)
+        registerReceiver(usbReceiver, filter)
+
+    }
+
+    /**
+     * アクセサリとの通信の権限を取得する
+     */
+    private val usbReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val action = intent?.action
+            when(action){
+                ACTION_USB_PERMISSION->{
+                    //接続されたアクセサリを表す UsbAccessory をインテントから取得できます
+                    val usbAccessory =
+                        intent?.getParcelableArrayExtra(UsbManager.EXTRA_DEVICE) as UsbAccessory
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (usbAccessory != null){
+                            readDevice()
+                        }else{
+                            var textview = findViewById<TextView>(R.id.textView)
+                            textview.text = "USB接続せれておりません！"
+                            Toast.makeText(this@MainActivity,"USB接続せれておりません",Toast.LENGTH_LONG).show()
+                        }
+                    }else{
+                        Toast.makeText(this@MainActivity,"権限がありません！",Toast.LENGTH_LONG).show()
+                    }
+                }
+                UsbManager.ACTION_USB_ACCESSORY_ATTACHED->{
+                    val device_add= intent.getParcelableArrayExtra(UsbManager.EXTRA_DEVICE)
+                    if (device_add!=null){
+                        redUDiskDevsList();
+                    }
+                }
+                UsbManager.ACTION_USB_ACCESSORY_DETACHED->{
+                    Toast.makeText(this@MainActivity,"USBを抜き出しました",Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    /**
+     * USBを読み込み
+     */
+    private fun readDevice() {
+        //Log.d(TAG, "openAccessory: $mAccessory")
+        fileDescriptor = usbManager?.openAccessory(accessory)
+        fileDescriptor?.fileDescriptor?.also { fd ->
+            inputStream = FileInputStream(fd)
+            outputStream = FileOutputStream(fd)
+            val thread = Thread(null, null, "AccessoryThread")
+            thread.start()
+        }
+    }
+    /**
+     *
+     */
+    private fun redUDiskDevsList(){
+
+    }
+}
